4275587efe23ef62f044356ed6858fc51b5af6e8
zhang <912305258@qq.com>
2019-12-12 18:04:04 +0800

    初始化项目

--- src/main/java/com/zhang/template/TemplateApplication.java
+++ src/main/java/com/zhang/template/TemplateApplication.java
@@ -1,13 +1,44 @@
 package com.zhang.template;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@MapperScan(value = "com.zhang.template.mapper")
 public class TemplateApplication {
+
+	private static String port;
+
+	public static String getPort() {
+		return port;
+	}
+
+	@Value("${server.port}")
+	public static void setPort(String port) {
+		TemplateApplication.port = port;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateApplication.class, args);
+	}
+
+	/**
+	 * 跨域支持
+	 * @return
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**");
+			}
+		};
 	}
 
 }
