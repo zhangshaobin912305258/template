b7a6e24dca6c872ddf7f650742180ab5792575b1
张绍彬 <912305258@qq.com>
2019-12-19 00:18:16 +0800

    初始化项目

--- src/main/java/com/zhang/template/TemplateApplication.java
+++ src/main/java/com/zhang/template/TemplateApplication.java
@@ -1,45 +1,15 @@
 package com.zhang.template;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan(value = "com.zhang.template.mapper")
 public class TemplateApplication {
 
-	private static String port;
-
-	@Value("${server.port}")
-	public void setPort(String port) {
-		TemplateApplication.port = port;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateApplication.class, args);
-		printServer();
-	}
-
-	public static void printServer() {
-		System.out.println("服务启动成功,访问地址: http://localhost:"+port);
-	}
-
-	/**
-	 * 跨域支持
-	 * @return
-	 */
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**");
-			}
-		};
 	}
 
 }
--- src/test/java/com/zhang/template/TemplateApplicationTests.java
+++ src/test/java/com/zhang/template/TemplateApplicationTests.java
@@ -1,13 +1,102 @@
 package com.zhang.template;
 
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class TemplateApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		// 代码生成器
+		AutoGenerator mpg = new AutoGenerator();
+
+		// 全局配置
+		GlobalConfig gc = new GlobalConfig();
+		String projectPath = System.getProperty("user.dir");
+		gc.setOutputDir(projectPath + "/src/main/java");
+		gc.setAuthor("zhang");
+		gc.setOpen(false);
+		gc.setActiveRecord(true);
+		// gc.setSwagger2(true); 实体属性 Swagger2 注解
+		mpg.setGlobalConfig(gc);
+
+		// 数据源配置
+		DataSourceConfig dsc = new DataSourceConfig();
+		dsc.setUrl("jdbc:mysql://localhost:3306/template?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8");
+		// dsc.setSchemaName("public");
+		dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+		dsc.setUsername("root");
+		dsc.setPassword("root");
+		mpg.setDataSource(dsc);
+
+		// 包配置
+		PackageConfig pc = new PackageConfig();
+		pc.setParent("com.zhang.template");
+		mpg.setPackageInfo(pc);
+
+		// 自定义配置
+		InjectionConfig cfg = new InjectionConfig() {
+			@Override
+			public void initMap() {
+				// to do nothing
+			}
+		};
+		// 如果模板引擎是 freemarker
+		//String templatePath = "/templates/mapper.xml.ftl";
+		// 如果模板引擎是 velocity
+		String templatePath = "/templates/mapper.xml.vm";
+
+		// 自定义输出配置
+		List<FileOutConfig> focList = new ArrayList<>();
+		// 自定义配置会被优先输出
+		focList.add(new FileOutConfig(templatePath) {
+			@Override
+			public String outputFile(TableInfo tableInfo) {
+				// 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+				return projectPath + "/src/main/resources/mapper/"+ tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+			}
+		});
+		cfg.setFileOutConfigList(focList);
+		mpg.setCfg(cfg);
+
+		// 配置模板
+		TemplateConfig templateConfig = new TemplateConfig();
+
+		templateConfig.setXml(null);
+		mpg.setTemplate(templateConfig);
+
+		// 策略配置
+		StrategyConfig strategy = new StrategyConfig();
+		strategy.setNaming(NamingStrategy.underline_to_camel);
+		strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+		//strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+		strategy.setEntityLombokModel(true);
+		//strategy.setRestControllerStyle(true);
+		// 公共父类
+		//strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
+		// 写于父类中的公共字段
+		//strategy.setSuperEntityColumns("id");
+		//strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+		//strategy.setControllerMappingHyphenStyle(true);
+		//strategy.setTablePrefix(pc.getModuleName() + "_");
+		mpg.setStrategy(strategy);
+		mpg.setTemplateEngine(new VelocityTemplateEngine());
+		mpg.execute();
 	}
 
 }
