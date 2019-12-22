c2877408ac4678f4a64d8213f0b8ad3913055fc4
张绍彬 <912305258@qq.com>
2019-12-23 00:59:40 +0800

    设置security工具类

---
+++ src/main/java/com/zhang/template/util/SecurityUtils.java
@@ -0,0 +1,55 @@
+package com.zhang.template.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Security相关操作
+ */
+public class SecurityUtils {
+
+	/**
+	 * 获取当前用户名
+	 * @return
+	 */
+	public static String getUsername() {
+		String username = null;
+		Authentication authentication = getAuthentication();
+		if(authentication != null) {
+			Object principal = authentication.getPrincipal();
+			if(principal != null && principal instanceof UserDetails) {
+				username = ((UserDetails) principal).getUsername();
+			}
+		}
+		return username;
+	}
+
+	/**
+	 * 获取用户名
+	 * @return
+	 */
+	public static String getUsername(Authentication authentication) {
+		String username = null;
+		if(authentication != null) {
+			Object principal = authentication.getPrincipal();
+			if(principal != null && principal instanceof UserDetails) {
+				username = ((UserDetails) principal).getUsername();
+			}
+		}
+		return username;
+	}
+
+	/**
+	 * 获取当前登录信息
+	 * @return
+	 */
+	public static Authentication getAuthentication() {
+		if(SecurityContextHolder.getContext() == null) {
+			return null;
+		}
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication;
+	}
+
+}
