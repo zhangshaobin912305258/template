d7edbe58c484f5d48436f459753beb12422a95be
张绍彬 <912305258@qq.com>
2020-03-31 08:57:37 +0800

    Default Changelist

--- src/main/java/com/zhang/template/config/AopConfig.java
+++ src/main/java/com/zhang/template/config/AopConfig.java
@@ -1,8 +1,8 @@
 package com.zhang.template.config;
 
-import com.zhang.template.util.DateUtil;
 import com.zhang.template.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -16,21 +16,21 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+import java.net.URLDecoder;
 
 @Aspect
 @Slf4j
 @Component
 public class AopConfig {
-    private  Map<String,Object> logDto;
     @Pointcut("execution(* com.zhang.template.controller..*(..)))")
-    private void logPointCut() {}
+    private void logPointCut() {
+    }
 
     @Around("logPointCut()")
     public Object logPrint(ProceedingJoinPoint point) throws Throwable {
-        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
-        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
-        HttpServletRequest request = sra.getRequest();
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
+        HttpServletRequest request = servletRequestAttributes.getRequest();
         String method = request.getMethod();
         String uri = request.getRequestURI();
         String queryString = request.getQueryString();
@@ -44,7 +44,7 @@ public class AopConfig {
             //获取请求参数集合并进行遍历拼接
             if (args.length > 0) {
                 if ("POST".equals(method)) {
-                    Object[] arguments  = new Object[args.length];
+                    Object[] arguments = new Object[args.length];
                     for (int i = 0; i < args.length; i++) {
                         if (args[i] instanceof ServletRequest || args[i] instanceof ServletResponse || args[i] instanceof MultipartFile) {
                             //ServletRequest不能序列化，从入参里排除，否则报异常：java.lang.IllegalStateException: It is illegal to call this method if the current request is not in asynchronous mode (i.e. isAsyncStarted() returns false)
@@ -63,13 +63,14 @@ public class AopConfig {
                 } else if ("GET".equals(method)) {
                     params = queryString;
                 }
-                //params = URLDecoder.decode(params,"UTF-8");
+                if (StringUtils.isNotEmpty(params)) {
+                    params = URLDecoder.decode(params, "UTF-8");
+                }
             }
-            log.info("requestMethod:{},请求地址:{},请求参数:{},响应:{},请求耗时:{}ms.", method , uri,  params,
-                    JsonUtil.bean2Json(result),(endTime - startTime));
-        }catch (Exception e){
+            log.info("requestMethod:{},请求地址:{},请求参数:{},响应:{},请求耗时:{}ms.", method, uri, params, JsonUtil.bean2Json(result), (endTime - startTime));
+        } catch (Exception e) {
             e.printStackTrace();
-            log.error("log error !!",e);
+            log.error("log error !!", e);
         }
         return result;
     }
--- src/main/java/com/zhang/template/controller/LoginController.java
+++ src/main/java/com/zhang/template/controller/LoginController.java
@@ -2,13 +2,12 @@ package com.zhang.template.controller;
 
 import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
-import com.zhang.template.service.UserServiceImpl;
+import com.zhang.template.service.impl.UserServiceImpl;
 import com.zhang.template.vo.LoginVo;
 import com.zhang.template.vo.Result;
 import com.zhang.template.vo.constEnum.ResultState;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,39 +23,44 @@ import java.io.IOException;
 @RestController
 public class LoginController {
 
-  @Autowired private Producer producer;
+    private final Producer producer;
 
-  @Autowired private UserServiceImpl userService;
+    private final UserServiceImpl userService;
+
+  public LoginController(Producer producer, UserServiceImpl userService) {
+    this.producer = producer;
+    this.userService = userService;
+  }
 
   @GetMapping("captcha.jpg")
-  public void captcha(HttpServletResponse response, HttpServletRequest request) throws IOException {
-    response.setHeader("Cache-Control", "no-store, no-cache");
-    response.setContentType("image/jpeg");
-    // 生成文字验证码
-    String text = producer.createText();
-    // 生成图片验证码
-    BufferedImage image = producer.createImage(text);
-    // 保存到验证码到 session
-    request.getSession().setAttribute(Constants.KAPTCHA_SESSION_KEY, text);
-    ServletOutputStream out = response.getOutputStream();
-    ImageIO.write(image, "jpg", out);
-    IOUtils.closeQuietly(out);
-  }
+    public void captcha(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setContentType("image/jpeg");
+        // 生成文字验证码
+        String text = producer.createText();
+        // 生成图片验证码
+        BufferedImage image = producer.createImage(text);
+        // 保存到验证码到 session
+        request.getSession().setAttribute(Constants.KAPTCHA_SESSION_KEY, text);
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(image, "jpg", out);
+        IOUtils.closeQuietly(out);
+    }
 
-  @PostMapping(value = "/login")
-  public Result login(@RequestBody LoginVo loginInfo, HttpServletRequest request) {
-    String captcha = loginInfo.getCaptcha();
-    if (StringUtils.isEmpty(captcha)) {
-      return Result.error(ResultState.INVALID_CAPTCHA);
+    @PostMapping(value = "/login")
+    public Result login(@RequestBody LoginVo loginInfo, HttpServletRequest request) {
+        String captcha = loginInfo.getCaptcha();
+        if (StringUtils.isEmpty(captcha)) {
+            return Result.error(ResultState.INVALID_CAPTCHA);
+        }
+        // 校验验证码
+        Object sessionCaptcha = request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
+        if (sessionCaptcha == null) {
+            return Result.error(ResultState.INVALID_CAPTCHA);
+        }
+        if (!captcha.equalsIgnoreCase((String) sessionCaptcha)) {
+            return Result.error(ResultState.INVALID_CAPTCHA);
+        }
+        return userService.login(loginInfo, request);
     }
-    // 校验验证码
-    Object sessionCaptcha = request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
-    if (sessionCaptcha == null) {
-      return Result.error(ResultState.INVALID_CAPTCHA);
-    }
-    if (!captcha.equalsIgnoreCase((String) sessionCaptcha)) {
-      return Result.error(ResultState.INVALID_CAPTCHA);
-    }
-    return userService.login(loginInfo, request);
-  }
 }
