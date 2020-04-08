f32aa250ff85fb119d7c8c8dc706fec922002971
张绍彬 <912305258@qq.com>
2020-04-08 11:12:48 +0800

    增加2行输出

--- src/main/java/com/zhang/template/controller/LoginController.java
+++ src/main/java/com/zhang/template/controller/LoginController.java
@@ -71,5 +71,7 @@ public class LoginController {
         System.out.println("222");
         System.out.println(333);
         System.out.println(444);
+        System.out.println(555);
+        System.out.println(666);
     }
 }
