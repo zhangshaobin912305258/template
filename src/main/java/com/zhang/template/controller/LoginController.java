1afd54ae9fba4de70212d5d43247959eda1aeb91
张绍彬 <912305258@qq.com>
2020-04-08 11:20:40 +0800

    修改2行

--- src/main/java/com/zhang/template/controller/LoginController.java
+++ src/main/java/com/zhang/template/controller/LoginController.java
@@ -69,8 +69,7 @@ public class LoginController {
 
         System.out.println("111");
         System.out.println("222");
-        System.out.println(333);
-        System.out.println(444);
+        System.out.println(555);
         System.out.println(777);
         System.out.println(888);
     }
